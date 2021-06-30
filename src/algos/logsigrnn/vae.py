5b1acf05d91645cb533cd67c271b507eff55c932
Konstantins Starovoitovs <starovoitovs@gmail.com>
2021-06-30 19:41:36 +0200

    VAE update

--- src/algos/logsigrnn/vae.py
+++ src/algos/logsigrnn/vae.py
@@ -8,6 +8,7 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Input, Dense, Lambda, Layer, Add, Multiply
 from tensorflow.keras.models import Model, Sequential
 
+
 def _foo(i):
 
     tups = list(combinations(range(34), TUPLE_SIZE))
@@ -26,7 +27,7 @@ def nll(y_true, y_pred):
 
     # keras.losses.binary_crossentropy gives the mean
     # over the last axis. we require the sum
-    return K.sum(K.binary_crossentropy(y_true, y_pred), axis=-1)
+    return K.mean(K.square(y_true - y_pred), axis=-1)
 
 
 class KLDivergenceLayer(Layer):
@@ -54,14 +55,14 @@ class KLDivergenceLayer(Layer):
 
 def build_vae_model():
 
-    original_dim = 561*12
-    intermediate_dim = 561 * 4
-    latent_dim = 256
-    epsilon_std = 1.0
+    original_dim = 102
+    intermediate_dim = 48
+    latent_dim = 4
+    epsilon_std = 0.1
 
     decoder = Sequential([
         Dense(intermediate_dim, input_dim=latent_dim, activation='relu'),
-        Dense(original_dim, activation='sigmoid')
+        Dense(original_dim, activation='linear')
     ])
 
     x = Input(shape=(original_dim,))
@@ -70,12 +71,12 @@ def build_vae_model():
     z_mu = Dense(latent_dim)(h)
     z_log_var = Dense(latent_dim)(h)
 
-    z_mu, z_log_var = KLDivergenceLayer()([z_mu, z_log_var])
-    z_sigma = Lambda(lambda t: K.exp(.5*t))(z_log_var)
+    kl_z_mu, kl_z_log_var = KLDivergenceLayer()([z_mu, z_log_var])
+    z_sigma = Lambda(lambda t: K.exp(.5*t))(kl_z_log_var)
 
     eps = K.random_normal(stddev=epsilon_std, shape=(K.shape(x)[0], latent_dim))
     z_eps = Multiply()([z_sigma, eps])
-    z = Add()([z_mu, z_eps])
+    z = Add()([kl_z_mu, z_eps])
 
     x_pred = decoder(z)
 
