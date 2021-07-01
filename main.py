5f5ff0aa9f21812a9dc9837447ba2ccbab5ef760
lenagal <85573174+lenagal@users.noreply.github.com>
2021-07-01 14:58:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from src.preprocessing.augmented_sig_transformer import AugmentedSigTransformer
 from src.algos.sig_classifier import SigClassifier
 from src.preprocessing.pre_normaliser import preNormaliser
-
+from src.algos.sigRNNlinconv import SigRNNLinConv
+from src.preprocessing.linconv_data import LinConvData
 
 class SkeletonBasedActionRecognition:
     '''
         A class for skeleton-based human action recognition on UAV.
-        Two methods: 1) '__init__': data preprocessing or loading, building the classifier. 
+        Two methods: 1) '__init__': data preprocessing or loading, building the classifier.
         ... A priori, several classifiers can be chosen and the device can be specified. 2) 'run' contains training
         ... and validation, as well as printing the confusion matrix.
     '''
     def __init__(self,algo='SigClassifier',batch_size=20,lr=0.0001,epochs=20,transform='example',load_data=False,
-            device='cuda',pad=True,centre=1,rotate=1,switchBody =True, eliminateSpikes = True, scale = 2, smoothen = True,debug=True):
-            
+            device='cuda',pad=True,centre=1,rotate=1,switchBody =True, eliminateSpikes = True, scale = 2, smoothen = True,debug=False):
+
             #algo='SigClassifier',batch_size=20,lr=0.0001,epochs=20,transform='example',load_data=True,
             #device='cuda',pad=True,centre=True,rotate=True,debug=False):
         '''
@@ -43,39 +44,50 @@ class SkeletonBasedActionRecognition:
             smoothen: whether to apply a savgol filter to smoothen the data or not, see ~/src/pre_processing/pre_normaliser.py
 
             debug: if True, only a small fraction of the frames and joints will be considered.
-            
+
             · Commands:
             Defines a suitable preNormaliser object, if data is not to be loaded.
             Defines the signature data wrapper (an AugmentedSigTransformer object) and the sig_classifier (a SigClassifier
             ... object) for the 'SigClassifier' algo with the specified parameters, see above
         '''
-        
+
         if not load_data:
-            pre_normaliser = preNormaliser(pad=pad,centre=centre,rotate=rotate,switchBody=switchBody,eliminateSpikes=eliminateSpikes,scale=scale,smoothen=smoothen)
+            print('Init pre_normaliser')
+            pre_normaliser = preNormaliser(pad=pad,centre=centre,rotate=rotate,switchBody=switchBody,eliminateSpikes=eliminateSpikes,scale=scale,parallel=False,smoothen=smoothen)
+            print('Init pre_normaliser finished')
         else:
             pre_normaliser = None
-            
+
         self.algo = algo
+
         if algo=='SigClassifier':
             sig_data_wrapper = AugmentedSigTransformer(pre_normaliser=pre_normaliser,transform=transform,
                     load_transform=load_data,debug=debug)
             self.sig_classifier = SigClassifier(
                     sig_data_wrapper=sig_data_wrapper,device=device,batch_size=batch_size,lr=lr,epochs=epochs)
+
+        if self.algo=='logsigrnn_linconv' :
+            sig_data_wrapper = LinConvData(pre_normaliser=pre_normaliser,transform=transform, load_transform=load_data,debug=debug)
+            self.sig_classifier = SigRNNLinConv(sig_data_wrapper=sig_data_wrapper,batch_size=batch_size,lr=lr,epochs=epochs)
+
         else:
             raise NotImplementedError
 
-    def run(self, render_plot: bool = False):
+    def run(self, render_plot: bool = True):
         '''
-            Performs training and validation. Prints the confusion matrix with respect to the validation set if 
+            Performs training and validation. Prints the confusion matrix with respect to the validation set if
             ... render_plot == True.
         '''
         if self.algo=='SigClassifier':
+            self.sig_classifier.run(render_plot=render_plot)
+        if self.algo=='logsigrnn_linconv':
             self.sig_classifier.run(render_plot=render_plot)
         else:
             raise NotImplementedError
 
 if __name__ == "__main__":
     print('Init obj.')
-    obj = SkeletonBasedActionRecognition()
+    obj = SkeletonBasedActionRecognition(algo='logsigrnn_linconv',batch_size=256,lr=0.001,epochs=60,transform='landmarks',load_data=False,
+            device='cuda',pad=True,centre=1,rotate=1,switchBody =True, eliminateSpikes = True, scale = 2, smoothen = False, debug=False)
     print('Run obj.')
     obj.run(render_plot=True)
