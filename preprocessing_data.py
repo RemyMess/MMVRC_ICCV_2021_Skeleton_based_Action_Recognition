a1b208102d0c3cc435171290d8e0b1a4af13f033
HannesKern <86022244+HannesKern@users.noreply.github.com>
2021-07-05 10:12:34 +0200

    Added time stamp

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,7 +1,10 @@
 import numpy as np
 from src.preprocessing.pre_normaliser import preNormaliser
 import pickle
+from datetime import datetime
 
+now = datetime.now()
+date = now.strftime("%d-%m-%Y-%H-%M")
 
 #creating train files:
 pre_normaliser = preNormaliser(pad=True, centre=1, rotate=0, switchBody =True, eliminateSpikes = True, scale = 2, parallel = True, smoothen = True,
@@ -10,8 +13,8 @@ data = pre_normaliser.train_prenorm_data
 labels = pre_normaliser.train_prenorm_label
 
 print( r"Saving preprocessed training data in src/data_grabbing/preprocessed_data" )
-np.save(r"src/data_grabbing/preprocessed_data/train_process_data.npy", data)
-np.save(r"src/data_grabbing/preprocessed_data/labels.npy", labels)
+np.save(r"src/data_grabbing/preprocessed_data/train_process_data"+date+".npy", data)
+np.save(r"src/data_grabbing/preprocessed_data/labels"+date+".npy", labels)
 
 #creating test files:
 pre_normaliser = preNormaliser(pad=True, centre=1, rotate=0, switchBody=True, eliminateSpikes=True, scale=2,parallel=True, smoothen=True,
@@ -20,8 +23,8 @@ data = pre_normaliser.test_prenorm_data
 names = pre_normaliser.test_filenames
 
 print( r"Saving preprocessed training data in src/data_grabbing/preprocessed_data" )
-np.save(r"src/data_grabbing/preprocessed_data/test_process_data.npy", data)
-with open(r"src/data_grabbing/preprocessed_data/test_filenames.pkl", 'wb') as f:
+np.save(r"src/data_grabbing/preprocessed_data/test_process_data"+date+".npy", data)
+with open(r"src/data_grabbing/preprocessed_data/test_filenames"+date+".pkl", 'wb') as f:
     pickle.dump(pre_normaliser.test_filenames, f)
 
 
